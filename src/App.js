37bcefc949ef52fc797227d97d9378933b716ddc
SMei201900 <smei201900@gmail.com>
2021-09-23 23:58:04 -0400

    Removed premade Code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import tempphoto from "./tempphoto.jpg";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+	return (
+		<div className="App">
+			<header className="App-header">
+				<h1>Cats</h1>
+				<img src={tempphoto} className="staringcat" alt="cat" width={500} />
+			</header>
+		</div>
+	);
 }
-
-export default App;
