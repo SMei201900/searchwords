b920424f231063bc0ee929fb7ea1a24747e76985
SMei201900 <smei201900@gmail.com>
2021-09-24 09:48:27 -0400

    Created Search Engine
    Still needs CSS

--- src/App.js
+++ src/App.js
@@ -1,41 +1,19 @@
 import tempphoto from "./tempphoto.jpg";
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 export default function App() {
 	return (
 		<div className="App">
-			<h1>Cats</h1>
-			<img src={tempphoto} className="staringcat" alt="cat" width={500} />
-			<div>
-				<span className="credit">
-					<a
-						className="codeSource"
-						href="https://github.com/SMei201900/searchwords"
-						target="_blank"
-						rel="noreferrer"
-					>
-						Open source code
-					</a>{" "}
-					by:{" "}
-					<a
-						className="profile"
-						href="https://www.linkedin.com/in/sally-mei-1a4439210/"
-						target="_blank"
-						rel="noreferrer"
-					>
-						Sally Mei
-					</a>{" "}
-					and{" "}
-					<a
-						className="hostSite"
-						href="https://vigorous-mcnulty-ccc11c.netlify.app/"
-						target="_blank"
-						rel="noreferrer"
-					>
-						hosted by Netlify
-					</a>
-				</span>
-			</div>{" "}
+			<div className="container">
+				<h1>Cats</h1>
+				<img src={tempphoto} className="cat" alt="cat" width={500} />
+				<main>
+					{" "}
+					<Dictionary />
+				</main>
+				<footer> Open Source Code by Sally Mei</footer>
+			</div>
 		</div>
 	);
 }
