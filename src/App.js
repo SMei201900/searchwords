85b4511c9859ab10e76af94981d189710603f392
SMei201900 <smei201900@gmail.com>
2021-09-24 00:04:46 -0400

    Added Credits

--- src/App.js
+++ src/App.js
@@ -4,10 +4,38 @@ import "./App.css";
 export default function App() {
 	return (
 		<div className="App">
-			<header className="App-header">
-				<h1>Cats</h1>
-				<img src={tempphoto} className="staringcat" alt="cat" width={500} />
-			</header>
+			<h1>Cats</h1>
+			<img src={tempphoto} className="staringcat" alt="cat" width={500} />
+			<div>
+				<span className="credit">
+					<a
+						className="codeSource"
+						href="https://github.com/SMei201900/searchwords"
+						target="_blank"
+						rel="noreferrer"
+					>
+						Open source code
+					</a>{" "}
+					by:{" "}
+					<a
+						className="profile"
+						href="https://www.linkedin.com/in/sally-mei-1a4439210/"
+						target="_blank"
+						rel="noreferrer"
+					>
+						Sally Mei
+					</a>{" "}
+					and{" "}
+					<a
+						className="hostSite"
+						href="https://vigorous-mcnulty-ccc11c.netlify.app/"
+						target="_blank"
+						rel="noreferrer"
+					>
+						hosted by Netlify
+					</a>
+				</span>
+			</div>{" "}
 		</div>
 	);
 }
