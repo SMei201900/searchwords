b1a7a0d04f41f355ec2213ef64c9655ed9dbcb71
SMei201900 <smei201900@gmail.com>
2021-10-02 02:41:46 -0400

    Removed

--- src/Meaning.js
+++ src/Meaning.js
@@ -11,7 +11,6 @@ export default function Meaning(props) {
 					<div key={index}>
 						<div className="definition">{definition.definition}</div>
 						<div className="example">{definition.example}</div>
-						<p> Synonyms </p>
 						<Synonym synonym={definition.synonyms} />
 					</div>
 				);
