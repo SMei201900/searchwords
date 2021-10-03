302f50d9757e211ee9c75fb5cc767c8f831f82e7
SMei201900 <smei201900@gmail.com>
2021-10-03 01:04:54 -0400

    Informed user when there is no audio for a word
    Related to Phonetics

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -2,13 +2,29 @@ import React from "react";
 import "./Phonetics.css";
 
 export default function Phonetic(props) {
-	return (
-		<div className="Phonetic">
-			<a href={props.phonetic.audio} target="_blank" rel="noreferrer">
-				Listen
-			</a>{" "}
-			{""}
-			<span className="pronounce"> {props.phonetic.text} </span>
-		</div>
-	);
+	function changeText(event) {
+		event.preventDefault();
+		alert("No audio");
+	}
+
+	if (props.phonetic.audio) {
+		return (
+			<div className="Phonetic">
+				<a href={props.phonetic.audio} target="_blank" rel="noreferrer">
+					Listen
+				</a>{" "}
+				{""}
+				<span className="pronounce"> {props.phonetic.text} </span>
+			</div>
+		);
+	} else {
+		return (
+			<div>
+				<button className="btn noAudio" onClick={changeText}>
+					Listen
+				</button>
+				<span className="pronounce"> {props.phonetic.text} </span>
+			</div>
+		);
+	}
 }
